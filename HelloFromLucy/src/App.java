1236313522cca4ffdc9da1059ea8dfe18b7635e6
Lucila Guzman Mijangos <guz20012@byui.edu>
2021-01-30 00:31:47 -0600

    Exclamation mark added

--- HelloFromLucy/src/App.java
+++ HelloFromLucy/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, my name is Lucila");
+        System.out.println("Hello, my name is Lucila!");
         System.out.println("Hello Lucila, my name is Miguel.");
     }
 }
